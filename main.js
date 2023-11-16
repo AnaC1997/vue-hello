5e86019bc237aecd55c162468dffda7f06447a45
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-11-16 14:47:21 +0100

    .
    .

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const messaggio = {
     data: function () {
         return {
             message: "Vue.js Framework ",
-            imageUrl: "https://www.renzocappelli.com/wp-content/uploads/2020/05/vue.jpg"
+            imageUrl: "vue.jpeg"
         }
     }
 };
